c18b2064c7e5cb36c02201b8b684778066ec2a54
mzzubair <130418671+mzzubair@users.noreply.github.com>
2024-01-16 20:03:12 +0000

    <+

--- 01-basic/Test.js
+++ 01-basic/Test.js
@@ -1,1 +1,16 @@
-console.log("Mohammed Zubair")
+// console.log("Mohammed Zubair")
+
+// console.log(+false)
+
+// let num1 ,num2 ,num3
+
+// num1 = num2 = num3 =2 + 2 /5 *5 -5
+
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+ 
+
+let = gameconut =100
+gameconut++;
+console.log(gameconut);
